23d8dd7ba5829efd4c97351b2bb61fabf26aa32f
Alexander Catchpole <alex@alexcatchpoledev.me>
2017-01-30 00:33:05 +0000

    Update properties of plugin to include routes used

--- src/plugins/demo.js
+++ src/plugins/demo.js
@@ -4,20 +4,43 @@ export class Plugin {
 		this.registerRoutes(Knekt);
 	}
 
+	//properties is how you provide information to the client.
 	properties = {
 		name: "My Plugin",
 		description: "A demo plugin",
 		website: "http://Knekt.co",
-		author: "Alex Catchpole"
+		author: "Alex Catchpole",
+		routes: [
+			{
+				route: '/my-plugin/test',
+				method: 'GET'
+			},
+			{
+				route: '/my-plugin/test',
+				method: 'POST',
+				parameters: [
+					{
+						demo: "string"
+					}
+				]
+			}
+		]
 	};
 
+	//Namespace your routes!
 	registerRoutes(Knekt) {
-		//Namespace your routes!
 		Knekt.addRoute({
     		method: 'GET',
     		path: '/my-plugin/test',
     		handler: function (request, reply) {
         		reply('Hello, world!');
+    		}
+		});
+		Knekt.addRoute({
+    		method: 'POST',
+    		path: '/my-plugin/test',
+    		handler: function (request, reply) {
+        		reply(request.payload);
     		}
 		});
 	}
