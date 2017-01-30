8e44baa3db6723dd83cf8bd5d9686c3d2eb33be1
Alexander Catchpole <alex@alexcatchpoledev.me>
2017-01-30 00:32:17 +0000

    Update to register descriptor route

--- src/lib/Knekt.js
+++ src/lib/Knekt.js
@@ -3,6 +3,9 @@
  */
  import Hapi from 'hapi';
 
+ //core routes
+ import Descriptor from './routes/descriptor';
+
  export class Knekt {
  	constructor() {
  		this._server = new Hapi.Server();
@@ -18,17 +21,22 @@
  			if (err) {
  				throw err;
  			}
+ 			this._registerCoreRoutes();
  			console.log(`Knekt running at ${this._server.info.uri} !`);
  		});
  	}
 
  	register(plugins) {
  		plugins.forEach((plugin) => {
- 			this.plugins.push(new plugin(this));
+ 			this.plugins.push(new plugin(this).properties);
  		});
  	}
 
  	addRoute(parameters) {
  		this._server.route(parameters);
+ 	}
+
+ 	_registerCoreRoutes() {
+ 		this._server.route(Descriptor(this));
  	}
  }
